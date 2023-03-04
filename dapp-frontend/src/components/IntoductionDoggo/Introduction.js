222473f7cdee1a9dec6d4290072bc24f2a6baa21
onuruci <onur.yuce@metu.edu.tr>
2023-03-04 14:53:41 +0300

    Update Introduction.js

--- dapp-frontend/src/components/IntoductionDoggo/Introduction.js
+++ dapp-frontend/src/components/IntoductionDoggo/Introduction.js
@@ -5,32 +5,18 @@ import "./styles.css";
 const Introduction = () => {
   return (
     <code className="introstyle">
-      <div>Interactive SVG NFT's running on Avalanche Fuji testnet</div>
+      <div>Connect applications with your Solana profile, secure your wallet with social recovery</div>
       <div>
-        Here we have three types of mintable DoGGo's, green, red and blue. You
-        can mint them freely.
+        Here you can build a Solana profile with your personal information.
       </div>
       <div>
-        Then using your NFT's you can breed new DoGGo's with secondary colors.
+        With one profile you can connect all websites using our system.
       </div>
       <div>
-        Feel free to test the application and reach your feedback to me. If you
-        have any fun ideas we can discuss and build together.
+        You account and wallet will be protected using social recovery
       </div>
-      <div>Smart Contract Addresses </div>
-      <code>
-        <div>DoGGO NFT Smart Contract:</div>
-        <div>
-          <a
-            target="_blank"
-            href="https://testnet.snowtrace.io/address/0xB19db005C446E59dE8726E06735C5e454956Cc89#code"
-          >
-            <div>0xB19db00...56Cc89</div>
-          </a>
-        </div>
-      </code>
-      <div>Contact Me: uci.onur@gmail.com</div>
-      <div>Onur</div>
+      <div>Contact Us: uci.onur@gmail.com</div>
+      <div>Onur, Tuna</div>
     </code>
   );
 };
